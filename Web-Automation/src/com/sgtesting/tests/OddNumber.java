da4c3f9b6c6e68fcb3ae9ec9e349dc2bc2fae3de
Ruhinaz <ruhinaz855@gmail.com>
2022-01-12 14:20:02 +0530

    OddNumber

---
+++ Web-Automation/src/com/sgtesting/tests/OddNumber.java
@@ -0,0 +1,15 @@
+package com.sgtesting.tests;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=30;i++)
+		{
+			if(i%3==0)
+			{
+				System.out.println("odd Numbers are:" +i);	
+			}
+		}
+	}
+
+}
