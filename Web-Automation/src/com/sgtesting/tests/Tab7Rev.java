ed6ea50850106bebc64cc580ca8477329f782de2
Ruhinaz <ruhinaz855@gmail.com>
2022-01-12 18:47:17 +0530

    Tab7Rev

---
+++ Web-Automation/src/com/sgtesting/tests/Tab7Rev.java
@@ -0,0 +1,12 @@
+package com.sgtesting.tests;
+
+public class Tab7Rev {
+
+	public static void main(String[] args) {
+		for(int i=10;i>=1;i--)
+		{
+			System.out.println("7"+"*"+i+"="+7*i);
+		}
+	}
+}
+
